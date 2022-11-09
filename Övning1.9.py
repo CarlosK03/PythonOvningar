32f2871b0478c2552d852c7ea5b20e948bbabe0b
Carlos <carloskarat@gmail.com>
2022-11-09 04:16:46 +0100

    1

---
+++ Övning1.9.py
@@ -0,0 +1,8 @@
+# Asking for the yearly investment, the annual interest rate, and the number of years. Then it is
+# calculating the value of the investment after the number of years.
+principal = eval(input("Enter the yearly investment: "))
+apr = eval(input("Enter the annual interest rate: "))
+years = eval(input("Enter the number of years: "))
+for i in range(years):
+    principal = principal * (1+apr)
+print("The value in 12 years is: ", principal)
