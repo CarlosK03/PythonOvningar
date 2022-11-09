ae1360641b2e55982037b03783f1ab3577353e5f
Carlos <carloskarat@gmail.com>
2022-11-09 10:47:05 +0100

    ja du undrar

---
+++ Övning2.2.py
@@ -0,0 +1,7 @@
+firstName=input("Please Input First Name: ")
+middleName=input("Please Input Middle Name: ")
+lastName=input("Please Input Last Name: ")
+
+print(firstName.lower())
+print(middleName.upper())
+print(lastName.capitalize())
