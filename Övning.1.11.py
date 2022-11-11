ca468ca25a09fa14cdb26a2b064ee89225358cf6
Carlos <carloskarat@gmail.com>
2022-11-11 08:31:35 +0100

    updated

--- Övning.1.11.py
+++ Övning.1.11.py
@@ -10,3 +10,17 @@ def calculate():
     print(f"{firstYear:.2f},{secondYear:.2f},{thirdYear:.2f}")
 
 calculate();
+
+""" 
+Simplified
+amount = float(input("amount:"))
+rate = 0.04
+balance1 = amount *(1+rate)
+print("Bal after 1 year: {}".format(round(balance1)))
+
+balance2 = balance1 *(1+rate)
+print("Bal after 2 year: {}".format(round(balance2)))
+
+balance3 = balance2 *(1+rate)
+print("Bal after 3 year: {}".format(round(balance3)))
+ """
