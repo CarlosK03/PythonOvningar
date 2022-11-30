7df5ca97d895eb9ed9a20e7728681fba2872bad5
Carlos <carloskarat@gmail.com>
2022-11-30 15:49:47 +0100

    d sasaas

--- Labb1.py
+++ Labb1.py
@@ -10,32 +10,48 @@
 #         if str(search) in line:
 #             print(line)
 
-def creates_file(n: int) -> None:
-    """
-    > This function creates a file with the name `table_<n^2>.txt` and writes the text `This is the
-    number <n^2>` to it
+# def creates_file(n: int) -> None:
+#     """
+#     > This function creates a file with the name `table_<n^2>.txt` and writes the text `This is the
+#     number <n^2>` to it
     
-    :param n: int
-    :type n: int
-    """
-    with open(f"table_{n**2}.txt", "w") as f:
-        f.write(f"This is the number {n**2}")
+#     :param n: int
+#     :type n: int
+#     """
+#     with open(f"table_{n**2}.txt", "w") as f:
+#         f.write(f"This is the number {n**2}")
 
 
-def reads_file(n: int) -> None:
-    """
-    It reads the last word of the first line of a file
+# def reads_file(n: int) -> None:
+#     """
+#     It reads the last word of the first line of a file
     
-    :param n: int - the number of rows and columns in the table
-    :type n: int
-    """
-    with open(f"table_{n**2}.txt", "r") as f:
-        context = f.readline().split()[-1]
-        print(f"{f.name}: {context}")
+#     :param n: int - the number of rows and columns in the table
+#     :type n: int
+#     """
+#     with open(f"table_{n**2}.txt", "r") as f:
+#         context = f.readline().split()[-1]
+#         print(f"{f.name}: {context}")
 
 
-# A way to run the code in the file.
+# # A way to run the code in the file.
+# if __name__ == '__main__':
+#     for number in range(1, 10 + 1):
+#         creates_file(number)
+#         reads_file(number)
+
+def individualizing_file(number: int) -> None:
+    increasing_number: int = number
+
+    for _ in range(10):
+        with open(f"file_{calc()}.txt", "w") as f:
+            f.write("This file has just been created")
+
+
+def calc():
+    for i in range(1, 11):
+        print(n,"X",i,"=",n * i)
+
 if __name__ == '__main__':
-    for number in range(1, 10 + 1):
-        creates_file(number)
-        reads_file(number)
+    n = int(input("Enter a number between 1-9: "))
+    individualizing_file(n)
